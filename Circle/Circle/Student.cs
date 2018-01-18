9fa622f3449de10cd77dd4fb8300c53ff45dfab9
anuarkk <35423131+anuarkk@users.noreply.github.com>
2018-01-19 00:37:55 +0600

    first lab

---
+++ Circle/Circle/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Student
+{
+    internal class Student
+    {
+        private string firstName;
+        private string lastName;
+        private int gpa;
+        private string studname;
+        private string studlast;
+        private double studgpa;
+
+        public Student()
+        {
+            firstName = "firstName";
+            lastName = "lastName";
+            gpa = 0;
+        }
+       
+
+        public Student(string studname, string studlast, double studgpa)
+        {
+            this.studname = studname;
+            this.studlast = studlast;
+            this.studgpa = studgpa;
+        }
+
+        public override string ToString()
+        {
+            return firstName + "_" + lastName + "_" + gpa;
+        }
+    }
+}
