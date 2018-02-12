2cee6659559308876179d97726cdd08614af1dee
anuarkk <35423131+anuarkk@users.noreply.github.com>
2018-02-12 23:53:03 +0600

    smth

---
+++ ExampleSnake/ExampleSnake/food.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleSnake
+{
+    class food
+    {
+        public Point location;
+        public char sign;
+        public ConsoleColor color;
+
+        public food()
+        {
+            color = ConsoleColor.Green;
+            sign = '&';
+            location = new Point(14, 7);
+        }
+        public void SetRandomPosition()
+        {
+            int x = new Random().Next(0, 20);
+            int y = new Random().Next(0, 20);
+
+            location = new Point(x, y);
+        }
+        public void Draw()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(location.x, location.y);
+            Console.Write(sign);
+        }
+    }
+}
